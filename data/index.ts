88afb96fe2caa15639fbce2c2811cc2a1d39f2bf
Fardinsk7 <itzfardinsk@gmail.com>
2024-11-03 16:18:21 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,240 @@
+export const navItems = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const textstacksIcons = [
+  "/re.svg",
+  "/next.svg",
+  "/expressjs.png",
+  "/nodejs.png",
+  "/mongoDB.png",
+  "/ts.svg",
+  "tail.svg",
+  "/vitejs.jpeg",
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I prioritize my work and collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working to become better and better",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Mistry Suggestion Social Media Web App",
+    des: "Mistry Suggestion is a web app where users can log in and send anonymoussuggestions to anyone, fostering honest feedback and open communication. Mistry Suggestion is a web app where users can log in and send anonymous suggestions to anyone, fostering honest feedback and open communication",
+    img: "/project1.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/nextauth.jpeg",
+      "/mongoDB.png",
+      "/resend.png",
+    ],
+    link: "https://mistry-suggestion-app-y5uo.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Cayman Laptop Ecommerce Laptop Website",
+    des: "Full-fledged eCommerce website with an admin product management system, login system, product filtering, real-time add-to-cart management, different product viewing settings, user-friendly UI and UX, fully responsive",
+    img: "/project2.png",
+    iconLists: ["/re.svg", "/nodejs.png", "/expressjs.png", "/mongoDB.png"],
+    link: "https://caymanlaptops.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Issue Tracking System with Chat Area and Activity Logs",
+    des: "This web app lets users create customizable issue forms with any input fields. Users can log data, track changes with activity logs, and manage issues effectively. An integrated chat and document-sharing feature further enhances collaboration on each issue form.",
+    img: "/project3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/mongoDB.png"],
+    link: "https://issue-tracking-system-delta.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Keyword Spy",
+    des: "Users can use this tool for keyword research. This tool helps to see the keyword volume, competition, CPC, and paid difficulty.",
+    img: "/project4.png",
+    iconLists: ["/re.svg", "/vitejs.jpeg", "/chakraui.png", "/rapidapi.png"],
+    link: "https://keywordspy.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Mern Stack Developer Intern",
+    desc: "Worked as a Mern Stack Developer and developed various Dashboard, features, Ui's using Mern, Nextjs and Typescript.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Developer Intern",
+    desc: "Designed and developed Web app features and Ui using HTML, CSS, Javascript and also React js",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Freelance App Dev Project",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Fardinsk7",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Fardinsk_",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/fardin-khan-78968126b/",
+  },
+];
